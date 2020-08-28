2e0ea24b4cc36719dabac19e5abe8d916ea86abd
Fracalossi Silvia <silvia.fracalossi@stud-inf.unibz.it>
2020-08-28 15:45:25 +0200

    Added all the retrieval queries to the H2 database

--- src/main/java/H2DataExtractor.java
+++ src/main/java/H2DataExtractor.java
@@ -14,15 +14,45 @@ public class H2DataExtractor {
         return stmt.executeQuery(sql);
     }
 
-    // Retrieving all the accounts
+    // Retrieving all the demography info
     public ResultSet getAllDemographyInfo () throws Exception {
         String sql =  "SELECT * FROM demographyinfo ORDER BY user;";
         return stmt.executeQuery(sql);
     }
 
-    // Retrieving all the accounts
+    // Retrieving all the questionnaire participants
+    public ResultSet getAllQuestionnaireParticipants () throws Exception {
+        String sql =  "SELECT * FROM questionnaireparticipants;";
+        return stmt.executeQuery(sql);
+    }
+
+    // Retrieving all the questionnaire responses info
+    public ResultSet getAllQuestionnaireResponses () throws Exception {
+        String sql =  "SELECT * FROM questionnaireresponses;";
+        return stmt.executeQuery(sql);
+    }
+
+    // Retrieving all the questionnaire
+    public ResultSet getAllQuestionnaires () throws Exception {
+        String sql =  "SELECT * FROM questionnaires;";
+        return stmt.executeQuery(sql);
+    }
+
+    // Retrieving all the questionnaire templates
     public ResultSet getAllQuestionnaireTemplates () throws Exception {
         String sql =  "SELECT * FROM questionnairetemplates;";
+        return stmt.executeQuery(sql);
+    }
+
+    // Retrieving all the questionnaire
+    public ResultSet getAllQuestionnaireWines () throws Exception {
+        String sql =  "SELECT * FROM questionnairewines;";
+        return stmt.executeQuery(sql);
+    }
+
+    // Retrieving all the wine participant assignment
+    public ResultSet getAllWineParticipantAssignment () throws Exception {
+        String sql =  "SELECT * FROM wineparticipantassignment;";
         return stmt.executeQuery(sql);
     }
 }
